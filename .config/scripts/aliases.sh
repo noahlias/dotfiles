bf729c2be2932f24fa6a1256208420e0af972f54
Noahlias <yygggg@foxmail.com>
2024-06-09 10:40:11 +0800

    Update fzf with `catppuccin`

--- .config/scripts/aliases.sh
+++ .config/scripts/aliases.sh
@@ -1,12 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-alias lvim="~/.local/bin/lvim"
 alias ds="du -sh * | sort -h"
 alias rfv="ripgrep_fzf_vim"
 alias fancy="fortune | cowsay | lolcat"
 alias hs="history_statistics"
 alias p="degit python-discord/code-jam-template"
-alias ls="exa -l -a --icons"
+alias ls="eza -l -a --icons"
 alias cat="bat -p --theme gruvbox-dark"
 alias ne="neofetch"
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
@@ -14,14 +13,14 @@ alias lg="lazygit"
 alias c="clear"
 alias o="onefetch"
 alias n="nvim"
-alias ff="fzf | xargs nvim"
+alias ff="fzf --height=40% --border --info inline-right  --marker ▏ --pointer ▌ --prompt '▌ '  \
+    --highlight-line --color gutter:-1,selected-bg:238,selected-fg:146,current-fg:189 | xargs nvim"
 alias goody2="goody2"
 #kitty
 alias s="kitty +kitten ssh"
 alias icat="kitty +kitten icat --align left"
 
 alias g++='g++ -std=c++20'
-
 alias gcllm='git diff --minimal --cached | \
     llm -t gitcommit > $(git rev-parse --git-dir)/COMMIT_EDITMSG && \
     git commit --verbose --edit --file=$(git rev-parse --git-dir)/COMMIT_EDITMSG'
@@ -31,3 +30,6 @@ alias gcllm='git diff --minimal --cached | \
 alias cab='conda activate base'
 alias ca='conda activate'
 alias freeze='freeze -t gruvbox'
+alias ncdu='ncdu --color dark'
+alias bo="brew outdated"
+alias y="yazi"
