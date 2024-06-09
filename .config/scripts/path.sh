f9d73886e4a4f3f58e5adaffe6990d84ec2bcd17
Noahlias <yygggg@foxmail.com>
2024-06-09 10:40:32 +0800

    Misc config

--- .config/scripts/path.sh
+++ .config/scripts/path.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-
+# NOTE: import for c/c++ development
 export CPATH="/opt/Homebrew/include"
 export LIBRARY_PATH="/opt/Homebrew/lib"
-
-#treesitter
-#export TREESITTER_LIBRARY='/opt/Homebrew/opt/tree-sitter/lib/libtree-sitter.a'
-#export TREESITTER_INCLUDE_DIR='/opt/Homebrew/opt/tree-sitter/include'
 
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export EDITOR="nvim"
 export BUN_INSTALL="/Users/alias/.bun"
 export FLUTTER="/Users/alias/Downloads/flutter"
-export R_HOME='/opt/Homebrew/Cellar/r/4.3.1/lib/R/'
 export PATH="$BUN_INSTALL/bin:$PATH:$FLUTTER/bin"
 export LLVM_PATH='/opt/Homebrew/opt/llvm/bin'
 export PATH=/Users/alias/.nimble/bin:$PATH
+export PATH="$LLVM_PATH:$PATH"
 [ -f "/Users/alias/.ghcup/env" ] && source "/Users/alias/.ghcup/env" # ghcup-env
 
 export GPG_TTY=$(tty)
 
-export DYLD_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_LIBRARY_PATH"
-export PATH="$LLVM_PATH:$PATH"
+#  TODO: for the image.nvim can change to the magick  <04/24, 2024, noahlias>
+
+# HACK: I use the hardcode dylib in luarocks config
 
 export OPENSSL_ROOT_DIR="/opt/Homebrew/opt/openssl"
 
@@ -34,7 +31,6 @@ export LESS="--RAW-CONTROL-CHARS"
 
 # Use colors for less, man, etc.
 [[ -f ~/.LESS_TERMCAP ]] && . ~/.LESS_TERMCAP
-
 
 #rye
 source "$HOME/.rye/env"
@@ -49,6 +45,36 @@ export PATH="/Users/alias/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
 #NOTE: matplotlib
 
 export MPLCONFIGDIR="/Users/alias/.config/matplotlib"
+# go
 export GOPATH="/Users/alias/go"
 export GOBIN="/Users/alias/go/bin"
 export PATH=$PATH:$GOPATH/bin
+
+#NOTE: catppuccin for fzf
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+# NOTE:gruvbox material dark
+# local color00='#202020'
+# local color01='#2a2827'
+# local color02='#504945'
+# local color03='#5a524c'
+# local color04='#bdae93'
+# local color05='#ddc7a1'
+# local color06='#ebdbb2'
+# local color07='#fbf1c7'
+# local color08='#ea6962'
+# local color09='#e78a4e'
+# local color0A='#d8a657'
+# local color0B='#a9b665'
+# local color0C='#89b482'
+# local color0D='#7daea3'
+# local color0E='#d3869b'
+# local color0F='#bd6f3e'
+#
+# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS""\
+#  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D""\
+#  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C""\
+#  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
